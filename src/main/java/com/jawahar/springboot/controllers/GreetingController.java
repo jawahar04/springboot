fd95b48c1920171071bbb9cf8d6d949f382e24ba
Jawahar Siva <jawahar04@gmail.com>
2017-03-17 16:13:43 -0400

    updated controller

--- src/main/java/com/jawahar/springboot/controllers/GreetingController.java
+++ src/main/java/com/jawahar/springboot/controllers/GreetingController.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.web.HttpEncodingAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,10 +27,21 @@ public class GreetingController {
 			nextId = BigInteger.ONE;
 			
 		}
-		greeting.setId(nextId);
-		nextId = nextId.add(BigInteger.ONE);
-		greetingMap.put(greeting.getId(), greeting);
-		return greeting;
+		if (greeting.getId() != null) {
+			Greeting g = greetingMap.get(greeting.getId());
+			if (g == null) {
+				return null;
+			}
+			g.setGreetingText(greeting.getGreetingText());
+			greetingMap.put(g.getId(), g);
+			return g;
+		}
+		else {
+			greeting.setId(nextId);
+			nextId = nextId.add(BigInteger.ONE);
+			greetingMap.put(greeting.getId(), greeting);
+			return greeting;
+		}
 	}
 	static {
 		Greeting g1 = new Greeting();
@@ -42,14 +54,14 @@ public class GreetingController {
 		
 	}
 	
-	@RequestMapping(value = "/greetings", method = RequestMethod.GET)
+	@RequestMapping(value = "/api/greetings", method = RequestMethod.GET)
 	public ResponseEntity<Collection<Greeting>> getGreetings() {
 		
 		return new ResponseEntity<Collection<Greeting>>(greetingMap.values(), HttpStatus.OK);
 		
 	}
 	
-	@RequestMapping(value="/greetings/{id}")
+	@RequestMapping(value="/api/greetings/{id}")
 	public ResponseEntity<Greeting> getGreeting(@PathVariable BigInteger id) {
 		Greeting g = greetingMap.get(id);
 		if (g == null)
@@ -57,5 +69,42 @@ public class GreetingController {
 		return new ResponseEntity<Greeting>(g, HttpStatus.OK);
 		
 	}
+	
+	@RequestMapping(value="/api/greetings", method = RequestMethod.POST)
+	public ResponseEntity<Greeting> createGreeting(@RequestBody Greeting greeting) {
+		System.out.println("createGreeting called: " + greeting.getGreetingText());
+		Greeting g = save (greeting);
+		
+		return new ResponseEntity<Greeting>(g, HttpStatus.CREATED);
+		
+	}
+	
+	// Note 1 - Since the request comes in as http://localhost/api/greetings/2, if {id} is not specified in RequestMapping
+	// this updateGreeting method isn't called.
+	// Note 2 - Now that we have established the need for {id} it should be a PathVariable and has to be first i.e. before
+	// RequestBody
+	@RequestMapping(value="/api/greetings/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Greeting> updateGreeting(@PathVariable BigInteger id, @RequestBody Greeting greeting) {
+		System.out.println("updateGreeting called: " + greeting.getGreetingText());
+		greeting.setId(id);
+		Greeting g = save (greeting);
+		if (g == null)
+			return new ResponseEntity<Greeting>(HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return new ResponseEntity<Greeting>(g, HttpStatus.OK);
+		
+	}
+	
+	@RequestMapping(value="/api/greetings/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Greeting> deleteGreeting(@PathVariable BigInteger id) {
+		
+		Greeting g =  greetingMap.remove(id);
+		if (g == null)
+			return new ResponseEntity<Greeting>(HttpStatus.NOT_FOUND);
+		return new ResponseEntity<Greeting>(HttpStatus.OK);
+		
+	}
+	
+	
 
 }
